453bebb3f8b0861871f589721ac35f25d7d79f81
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-09-01 23:02:12 +0530

    Possibility of Winning Heads & Tails

---
+++ FlipCoinUc2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+read -p "Enter How many times you want to flip:" n
+flips=1
+count=0
+heads=0
+tails=0
+while [[ $flips -le n ]]
+do
+     coin=$((1+RANDOM%2))
+     flips=`expr $flips + 1`;
+     if [ $coin -eq 1 ]
+     then
+        echo "Heads";
+        heads=`expr $heads + 1`;
+     else [ $coin -eq 0 ]
+         echo "Tails";
+         tails=`expr $tails + 1`;
+      fi
+done
+echo "Head Won $heads Times" "and" "Tail won $tails Times."
+exit;
